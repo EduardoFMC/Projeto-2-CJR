67d273dbbe778e1749a9efa964beb479e800b0fd
felipeonf <felipeeflorentino@gmail.com>
2022-04-19 15:47:22 -0300

    include: configuracao do header e testes de rendering

---
+++ dudu/src/components/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Header () {
+    const element = (
+        <header>
+        <div class="options">
+            <h3>dinnerdash</h3>
+            <nav>
+                <a href="#">Backoffice</a>
+                <a href="#">Perfil</a>
+                <a href="#">Sair</a>
+            </nav>
+        </div>
+        <div>
+            <h1>Backoffice</h1>
+            <p>Gerencie pedidos, refeições e mais.</p>
+        </div>
+    </header>
+    );
+    return (
+        (element)
+    );
+
+    
+}
+
--- dudu/src/index.js
+++ dudu/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/header.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// criar os componentes da pagina
+// implementar os componentes
+// fazer os algoritmos de exclusão e paginação da tabela
+// estilizar melhor (ver styled components)
+
+
+function App () {
+  return (
+  <Header/>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
